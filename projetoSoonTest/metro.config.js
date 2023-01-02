d800b6c7988b9904da2a4b54e53c6de6981605f6
Andre Alves <andrealvesdev@gmail.com>
2023-01-02 15:21:07 -0300

    chore: add lib react-native-svg and svg transform

--- projetoSoonTest/metro.config.js
+++ projetoSoonTest/metro.config.js
@@ -5,13 +5,29 @@
  * @format
  */
 
+const { getDefaultConfig } = require('metro-config');
+
 module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
+  transformer: {},
 };
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
