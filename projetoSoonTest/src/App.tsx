cd85f93b9e34241a8a3170c1251eefb6c7dfa6b8
Andre Alves <andrealvesdev@gmail.com>
2023-01-09 23:24:30 -0300

    chore: app provider

--- projetoSoonTest/src/App.tsx
+++ projetoSoonTest/src/App.tsx
@@ -1,7 +1,13 @@
+import React from 'react';
 import Main from '~/screens/Main';
+import AppProvider from './hooks';
 
 const App = () => {
-  return <Main />;
+  return (
+    <AppProvider>
+      <Main />
+    </AppProvider>
+  );
 };
 
 export default App;
